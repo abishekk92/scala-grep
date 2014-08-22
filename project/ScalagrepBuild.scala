9b8ae14cf66cbe9b8dff529708c6a77c3474ed62
abishekk92 <bhat.abishek@gmail.com>
2014-08-23 01:40:17 +0530

    Add scala-test & SnakeYAML to libraryDependencies

--- project/ScalagrepBuild.scala
+++ project/ScalagrepBuild.scala
@@ -10,8 +10,9 @@ object ScalagrepBuild extends Build {
       name := "scala-grep",
       organization := "com.abishekk92",
       version := "0.1-SNAPSHOT",
-      scalaVersion := "2.10.4"
-      // add other settings here
+      scalaVersion := "2.10.4",
+      libraryDependencies ++= Seq("org.scalatest" % "scalatest_2.10" % "2.2.0" % "test",
+                                  "org.yaml" % "snakeyaml" % "1.13")
     )
   )
 }
